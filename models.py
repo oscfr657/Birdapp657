2688a3bc27622cbb5971d07a2061942e71c8b738
oscfr <oscfr657@gmail.com>
2019-08-25 13:58:35 +0200

    !Migrations! Added a bird form page with a hidden honeypot field. Now showing go_live_at or first_published_at date. Some css improvements.

--- models.py
+++ models.py
@@ -1,21 +1,28 @@
 
 from django.db import models
 from django.shortcuts import render
+from django import forms
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core import blocks
 from wagtail.search.models import Query
 from wagtail.search import index
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel, StreamFieldPanel, InlinePanel )
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from wagtail.contrib.forms.models import AbstractForm, AbstractFormField
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
+
+from modelcluster.fields import ParentalKey
 
 from .forms import SearchBirdForm
 from .blocks import BirdCodeBlock, RacerBirdBlock
 
 
-class BirdBasePage(Page):  # TODO: Rename to BaseBirdPage
+class BirdMixin(models.Model):
     author = models.CharField(max_length=255, blank=True, null=True)
     coverImage = models.ForeignKey(
         'wagtailimages.Image',
@@ -33,16 +40,24 @@ class BirdBasePage(Page):  # TODO: Rename to BaseBirdPage
             'link', 'document-link', 'blockquote']
             )
 
-    search_fields = Page.search_fields + [
+    class Meta:
+        abstract = True
+
+    search_fields = [
         index.SearchField('intro'),
         #index.FilterField('author'),
     ]
 
-    content_panels = Page.content_panels + [
+    content_panels = [
         FieldPanel('author'),
         ImageChooserPanel('coverImage'),
         FieldPanel('intro', classname="full")
     ]
+
+class BirdBasePage(Page, BirdMixin):  # TODO: Rename to CollectionBirdPage ?
+    search_fields = Page.search_fields + BirdMixin.search_fields
+
+    content_panels = Page.content_panels + BirdMixin.content_panels
 
     def get_context(self, request):
         context = super().get_context(request)
@@ -71,11 +86,11 @@ class BirdPage(BirdBasePage):
         ('racer', RacerBirdBlock(required=False, null=True)),
     ], blank=True, null=True)
 
-    search_fields = BirdBasePage.search_fields + [
+    search_fields = Page.search_fields + BirdMixin.search_fields + [
         index.SearchField('body'),
         #index.FilterField('author'),
     ]
-    content_panels = BirdBasePage.content_panels + [
+    content_panels = Page.content_panels + BirdMixin.content_panels +[
         StreamFieldPanel('body'),
     ]
 
@@ -83,7 +98,7 @@ class BirdPage(BirdBasePage):
 class RawBirdPage(BirdBasePage):
     html = models.TextField(blank=True, null=True)
     
-    content_panels = BirdBasePage.content_panels + [
+    content_panels = Page.content_panels + BirdMixin.content_panels + [
         FieldPanel('html', classname="full"),
     ]
 
@@ -111,3 +126,52 @@ class SearchBirdPage(Page):
             'form': form,
             'search_results': search_results,
         })
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'FormBirdPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields')
+        
+
+class FormBirdPage(AbstractForm, BirdMixin):
+    thank_you_text = RichTextField(
+        blank=True, null=True,
+        features=[
+            'h2', 'h3', 'h4',
+            'bold', 'italic',
+            'superscript', 'subscript', 'strikethrough'
+            'ol', 'ul', 'hr',
+            'link', 'document-link',
+            'blockquote', 'embed', 'image']
+            )
+
+    content_panels = BirdMixin.content_panels + AbstractForm.content_panels + [
+            FormSubmissionsPanel(),
+            InlinePanel('form_fields', label="Form fields"),
+            FieldPanel('thank_you_text', classname="full"),
+        ]
+
+    def serve(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = self.get_form(request.POST, request.FILES, page=self, user=request.user)
+            if form.data["bird_pot"]:
+                return self.render_landing_page(request, None, *args, **kwargs)
+            if form.is_valid():
+                form_submission = self.process_form_submission(form)
+                return self.render_landing_page(request, form_submission, *args, **kwargs)
+        else:
+            form = self.get_form(page=self, user=request.user)
+            form.fields["bird_pot"] = forms.CharField(
+                # initial='bird' % time.now().format(),  # Time of creation and check time at submission ?
+                # empty_value='bird',  # Time of creation and check time at submission ?
+                # validators=[validate_honey_pot, forms.CharField.default_validators],  # Does not work. :/
+                required=False)
+        context = self.get_context(request)
+        context['form'] = form
+        return render(
+            request,
+            self.get_template(request),
+            context
+        )
