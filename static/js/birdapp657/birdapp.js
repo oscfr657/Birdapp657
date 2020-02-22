35ffe9bd91d2c6f124bf0d48735867e1e7a92235
oscfr <oscfr657@gmail.com>
2020-02-22 18:03:06 +0100

    Added a mobile menu dropdown.

--- static/js/birdapp657/birdapp.js
+++ static/js/birdapp657/birdapp.js
@@ -1,10 +1,14 @@
 
 
 var prevScrollpos = window.pageYOffset;
+var currentScrollPos = window.pageYOffset;
 var wrapers = document.getElementsByClassName("bird_top_wraper");
 var transparent = wrapers[0].classList.contains('transparent');
+var active_menu = false;
+
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
+    currentScrollPos = window.pageYOffset;
+    if (active_menu) return;
     if (prevScrollpos > currentScrollPos) {
         wrapers[0].style.top = '0px';
         if (transparent) {
@@ -19,4 +23,21 @@ window.onscroll = function() {
         }
     }
     prevScrollpos = currentScrollPos;
+}
+
+document.getElementById("bird_mobile_menu_dropdown").onclick = function() {
+    if (!active_menu) {
+        active_menu = true;
+        if (transparent) {
+            wrapers[0].classList.remove('transparent');
+        }
+        document.getElementById("bird_mobile_menu_dropdown").classList.add('active');
+    } else {
+        active_menu = false;
+        if (transparent && currentScrollPos == 0) {
+            wrapers[0].classList.add('transparent');
+        }
+        document.getElementById("bird_mobile_menu_dropdown").classList.remove('active');
+    }
+    return false;
 }
