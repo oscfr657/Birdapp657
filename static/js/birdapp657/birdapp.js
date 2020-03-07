b6c3afaadc606fd38ecd42b75c0fa4061a037fbe
oscfr <oscfr657@gmail.com>
2020-03-07 23:32:05 +0100

    transparent breadcrumbs fix. mobile_menu_dropdown fix.

--- static/js/birdapp657/birdapp.js
+++ static/js/birdapp657/birdapp.js
@@ -5,8 +5,13 @@ var currentScrollPos = window.pageYOffset;
 var wrapers = document.getElementsByClassName("bird_top_wraper");
 var transparent = wrapers[0].classList.contains('transparent');
 var active_menu = true;
+var active_dropdown = false;
 
-window.onscroll = function() {
+window.onscroll = function(e) {
+    if (active_dropdown) {
+        e.preventDefault();
+        return false;
+    }
     currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         if (!active_menu) {
@@ -21,25 +26,27 @@ window.onscroll = function() {
             wrapers[0].style.top = '-114px';
         }
         active_menu = false;
+        active_dropdown = false;
     }
     if (currentScrollPos == 0 & prevScrollpos != currentScrollPos) {
         if (transparent) {
             wrapers[0].classList.add('transparent');
         }
         active_menu = true;
+        active_dropdown = true;
     }
     prevScrollpos = currentScrollPos;
 }
 
 document.getElementById("bird_mobile_menu_dropdown").onclick = function() {
-    if (!active_menu) {
-        active_menu = true;
+    if (!active_dropdown) {
+        active_dropdown = true;
         if (transparent) {
             wrapers[0].classList.remove('transparent');
         }
         document.getElementById("bird_mobile_menu_dropdown").classList.add('active');
     } else {
-        active_menu = false;
+        active_dropdown = false;
         if (transparent && currentScrollPos == 0) {
             wrapers[0].classList.add('transparent');
         }
