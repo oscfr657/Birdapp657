c387b9414f6e6f6831795e961326037e1ac3d605
Oscfr <oscfr657@gmail.com>
2021-01-07 14:50:01 +0100

    Refactory of transparent header. Hero width CSS improvement.

--- static/js/birdapp657/birdapp.js
+++ static/js/birdapp657/birdapp.js
@@ -2,8 +2,9 @@
 
 var prevScrollpos = window.pageYOffset;
 var currentScrollPos = window.pageYOffset;
-var wrapers = document.getElementsByClassName("bird_top_wraper");
-var transparent = wrapers[0].classList.contains('transparent');
+var wraper = document.getElementsByClassName("bird_top_wraper")[0];
+var wraper_list = wraper.getElementsByClassName("bird_menu_child");
+var transparent = document.body.classList.contains('transparent_header');
 var active_menu = true;
 var active_dropdown = false;
 
@@ -16,22 +17,72 @@ window.onscroll = function(e) {
     if (prevScrollpos > currentScrollPos) {
         if (!active_menu) {
             active_menu = true;
-            wrapers[0].style.visibility = 'visible';
-            wrapers[0].style.opacity = 1;
+            wraper.style.visibility = 'visible';
+            wraper.style.opacity = 1;
+            wraper.style.backgroundColor = 'white';
             if (transparent) {
-                wrapers[0].classList.remove('transparent');
+                document.getElementById("bird_mobile_menu_dropdown").style.border = 'solid 5px black';
+                wraper.getElementsByClassName("bird_logo")[0].getElementsByTagName('a')[0].style.color = 'black';
+                for (let el of wraper_list) {
+                    el.getElementsByTagName('a')[0].style.color = 'black';
+                    if (el.getElementsByClassName('active').length > 0) {
+                        el.getElementsByClassName('active')[0].style.setProperty('color', 'white');
+                    }
+                    el.addEventListener('mouseover',function(){
+                        el.getElementsByTagName('a')[0].style.color = 'white';
+                        el.getElementsByTagName('a')[0].style.backgroundColor = 'black';
+                        if (el.getElementsByClassName('active').length > 0) {
+                            el.getElementsByClassName('active')[0].style.color = 'white';
+                            el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                        }
+                    })
+                     el.addEventListener('mouseleave',function(){
+                        el.getElementsByTagName('a')[0].style.color = 'black';
+                        el.getElementsByTagName('a')[0].style.backgroundColor = 'white';
+                        if (el.getElementsByClassName('active').length > 0) {
+                            el.getElementsByClassName('active')[0].style.color = 'white';
+                            el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                        }
+                    })
+                }
             }
         }
     } else {
         if (active_menu) {
-            wrapers[0].style.visibility = 'hidden';
-            wrapers[0].style.opacity = 0;
+            wraper.style.visibility = 'hidden';
+            wraper.style.opacity = 0;
         }
         active_menu = false;
     }
     if (currentScrollPos == 0 & prevScrollpos != currentScrollPos) {
         if (transparent) {
-            wrapers[0].classList.add('transparent');
+            wraper.style.backgroundColor = 'transparent';
+            wraper.getElementsByClassName("bird_logo")[0].getElementsByTagName('a')[0].style.color = 'white';
+            document.getElementById("bird_mobile_menu_dropdown").style.border = 'solid 5px white';
+            for (let el of wraper_list) {
+                el.getElementsByTagName('a')[0].style.color = 'white';
+                el.getElementsByTagName('a')[0].style.backgroundColor = 'transparent';
+                if (el.getElementsByClassName('active').length > 0) {
+                    el.getElementsByClassName('active')[0].style.color = 'white';
+                    el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                }
+                el.addEventListener('mouseover',function(){
+                    el.getElementsByTagName('a')[0].style.color = 'white';
+                    el.getElementsByTagName('a')[0].style.backgroundColor = 'black';
+                    if (el.getElementsByClassName('active').length > 0) {
+                        el.getElementsByClassName('active')[0].style.color = 'white';
+                        el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                    }
+                })
+                 el.addEventListener('mouseleave',function(){
+                    el.getElementsByTagName('a')[0].style.color = 'white';
+                    el.getElementsByTagName('a')[0].style.backgroundColor = 'transparent';
+                    if (el.getElementsByClassName('active').length > 0) {
+                        el.getElementsByClassName('active')[0].style.color = 'white';
+                        el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                    }
+                })
+            }
         }
         active_menu = true;
     }
@@ -41,14 +92,40 @@ window.onscroll = function(e) {
 document.getElementById("bird_mobile_menu_dropdown").onclick = function() {
     if (!active_dropdown) {
         active_dropdown = true;
-        if (transparent) {
-            wrapers[0].classList.remove('transparent');
-        }
+        wraper.style.backgroundColor = 'white';
+        wraper.getElementsByClassName("bird_logo")[0].getElementsByTagName('a')[0].style.color = 'black';
+        document.getElementById("bird_mobile_menu_dropdown").style.border = 'solid 5px black';
         document.getElementById("bird_mobile_menu_dropdown").classList.add('active');
+        for (let el of wraper_list) {
+            el.getElementsByTagName('a')[0].style.color = 'black';
+            el.getElementsByTagName('a')[0].style.backgroundColor = 'white';
+            if (el.getElementsByClassName('active').length > 0) {
+                el.getElementsByClassName('active')[0].style.color = 'white';
+                el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+            }
+            el.addEventListener('mouseover',function(){
+                el.getElementsByTagName('a')[0].style.color = 'white';
+                el.getElementsByTagName('a')[0].style.backgroundColor = 'black';
+                if (el.getElementsByClassName('active').length > 0) {
+                    el.getElementsByClassName('active')[0].style.color = 'white';
+                    el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                }
+            })
+             el.addEventListener('mouseleave',function(){
+                el.getElementsByTagName('a')[0].style.color = 'black';
+                el.getElementsByTagName('a')[0].style.backgroundColor = 'white';
+                if (el.getElementsByClassName('active').length > 0) {
+                    el.getElementsByClassName('active')[0].style.color = 'white';
+                    el.getElementsByClassName('active')[0].style.backgroundColor = 'black';
+                }
+            })
+        }
     } else {
         active_dropdown = false;
         if (transparent && currentScrollPos == 0) {
-            wrapers[0].classList.add('transparent');
+            document.getElementById("bird_mobile_menu_dropdown").style.border = 'solid 5px white';
+            wraper.style.backgroundColor = 'transparent';
+            wraper.getElementsByClassName("bird_logo")[0].getElementsByTagName('a')[0].style.setProperty('color', 'white');
         }
         document.getElementById("bird_mobile_menu_dropdown").classList.remove('active');
     }
