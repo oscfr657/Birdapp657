0a33910aad6b5062c3f0f8c4a575f9abc8e64d61
Oscfr <oscfr657@gmail.com>
2021-12-02 01:54:41 +0100

    Build 0.1.1a2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="Birdapp657",
-    version='0.1.1a1',
+    version='0.1.1a2',
     description='A small Wagtail app.',
     long_description=long_description,
     long_description_content_type='text/markdown',
